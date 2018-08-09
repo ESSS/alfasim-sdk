399c55361b9dd45a504becdc41c87e826e77f70b
William Jamir Silva <williamjamir@gmail.com>
2018-08-09 15:57:57 -0300

    Add package command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,6 +9,39 @@ def test_command_line_interface():
     assert '--help  Show this message and exit.' in help_result.output
 
 
+def test_command_package(tmpdir):
+    import sys
+    from pathlib import Path
+    runner = CliRunner()
+    plugin_dir = Path(tmpdir / 'Acme')
+    artifacts_dir = plugin_dir / 'artifacts'
+
+    lib_name = 'acme.dll' if sys.platform == 'win32' else 'libacme.so'
+    lib_file = artifacts_dir / f'{lib_name}'
+
+    runner.invoke(cli.main, [
+        'template',
+        '--plugin-name=Acme',
+        '--shared-lib-name=acme',
+        '--author-email=acme@acme.com',
+        '--author-name=ACME',
+        f'--dst={Path(tmpdir)}',
+    ])
+
+    artifacts_dir.mkdir()
+    lib_file.write_text('', encoding='utf-8')
+
+    result = runner.invoke(cli.main, [
+        'package',
+        '--package-name=Acme',
+        f'--plugin-dir={plugin_dir}',
+        f'--dst={plugin_dir}',
+    ])
+
+    assert result.output == ''
+    assert result.exit_code == 0
+
+
 def test_command_template(tmpdir):
     from pathlib import Path
     runner = CliRunner()
@@ -23,16 +56,13 @@ def test_command_template(tmpdir):
     assert result.output == ''
     assert result.exit_code == 0
 
-def test_compile_command(tmpdir, mocker):
-    import os
-    mocker.patch.object(os, 'getcwd', return_value=tmpdir)
 
+def test_compile_command(tmpdir):
     runner = CliRunner()
-    result = runner.invoke(cli.main, ['compile', '--src', tmpdir])
+    result = runner.invoke(cli.main, ['compile', '--plugin-dir', tmpdir])
     assert f"Was not possible to find a build.py file in {tmpdir}" == result.exception.args[0]
 
     (tmpdir / 'build.py').write_text(data='', encoding='utf-8')
-    result = runner.invoke(cli.main, ['compile', '--src', tmpdir])
+    result = runner.invoke(cli.main, ['compile', '--plugin-dir', tmpdir])
     assert result.exception is None
     assert result.output == ''
-
