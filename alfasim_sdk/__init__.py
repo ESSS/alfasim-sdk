fc57d110a1be39c5c4d6d6300731d25319779c2a
William Jamir Silva <williamjamir@gmail.com>
2018-09-05 14:08:31 -0300

    Add hook_specs_gui

--- alfasim_sdk/__init__.py
+++ alfasim_sdk/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for alfasim-sdk."""
+import pluggy
 
 __author__ = """ESSS"""
 __email__ = 'foss@esss.co'
 __version__ = '0.1.0'
+
+hookimpl = pluggy.HookimplMarker("alfasim")
