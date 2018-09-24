3747d26553fd5b59460b7986a8cedd8f6d5599af
William Jamir Silva <williamjamir@gmail.com>
2018-09-24 17:47:18 -0300

    Rename parameters from container_model and data_model to lower case

--- alfasim_sdk/data_model.py
+++ alfasim_sdk/data_model.py
@@ -6,7 +6,7 @@ import attr
 from alfasim_sdk.data_types import BaseField
 
 
-def container_model(*, MODEL: type, CAPTION: str, ICON: Optional[str]):
+def container_model(*, model: type, caption: str, icon: Optional[str]):
     """
     Container model is a container object that keeps together many different properties.
     Similar to "data_model", the "container_model" can hold properties that are required
@@ -17,27 +17,27 @@ def container_model(*, MODEL: type, CAPTION: str, ICON: Optional[str]):
     for this user-declared model.
 
     1) Application Required:
-        - CAPTION: A text to be displayed
-        - ICON: Name of the icon available at resource folder to be used over the TreeStructure
-        - MODEL: A reference for a user-declared class that has the @data_model decorator.
+        - caption: A text to be displayed
+        - icon: Name of the icon available at resource folder to be used over the TreeStructure
+        - model: A reference for a user-declared class that has the @data_model decorator.
 
     2) User described:
        Check the module alfasim_sdk.data_types to verify all properties that the user can describe
     """
 
     def apply(class_):
-        setattr(class_, 'MODEL', attr.ib(default=MODEL))
+        setattr(class_, 'model', attr.ib(default=model))
 
         @functools.wraps(class_)
-        def wrap_class(class_, CAPTION, ICON):
-            return _wrap(CAPTION, ICON, class_)
+        def wrap_class(class_, caption, icon):
+            return _wrap(caption, icon, class_)
 
-        return wrap_class(class_, CAPTION, ICON)
+        return wrap_class(class_, caption, icon)
 
     return apply
 
 
-def data_model(*, CAPTION: str, ICON: Optional[str]=None):
+def data_model(*, caption: str, icon: Optional[str]=None):
     """
     Data model is a container object that keeps together many different properties.
     There are two kinds of properties that could be used with the "data_model" object:
@@ -45,26 +45,26 @@ def data_model(*, CAPTION: str, ICON: Optional[str]=None):
         2) User described.
 
     1) Application Required:
-        - CAPTION: A text to be displayed
-        - ICON: Name of the icon available at resource folder to be used over the TreeStructure
+        - caption: A text to be displayed
+        - icon: Name of the icon available at resource folder to be used over the TreeStructure
 
     2) User described:
        Check the module alfasim_sdk.data_types to verify all properties that the user can describe
     """
 
     def apply(class_: type):
-        setattr(class_, 'MODEL', attr.ib(default=None))
+        setattr(class_, 'model', attr.ib(default=None))
 
         @functools.wraps(class_)
-        def wrap_class(class_: type, CAPTION: str, ICON: Optional[str]):
-            return _wrap(CAPTION, ICON, class_)
+        def wrap_class(class_: type, caption: str, icon: Optional[str]):
+            return _wrap(caption, icon, class_)
 
-        return wrap_class(class_, CAPTION, ICON)
+        return wrap_class(class_, caption, icon)
 
     return apply
 
 
-def _wrap(CAPTION: str, ICON: Optional[str], class_: type):
+def _wrap(caption: str, icon: Optional[str], class_: type):
     for name in dir(class_):
         value = getattr(class_, name)
 
@@ -73,6 +73,6 @@ def _wrap(CAPTION: str, ICON: Optional[str], class_: type):
                 continue
             new_value = attr.ib(default=value)
             setattr(class_, name, new_value)
-    setattr(class_, 'CAPTION', attr.ib(default=CAPTION))
-    setattr(class_, 'ICON', attr.ib(default=ICON))
+    setattr(class_, 'caption', attr.ib(default=caption))
+    setattr(class_, 'icon', attr.ib(default=icon))
     return attr.s(class_)
--- tests/test_data_models.py
+++ tests/test_data_models.py
@@ -9,7 +9,7 @@ def user_data_model():
     class ValidType(BaseField):
         pass
 
-    @data_model(ICON="model.png", CAPTION='PLUGIN DEVELOPER MODEL')
+    @data_model(icon="model.png", caption='PLUGIN DEVELOPER MODEL')
     class Model:
         valid_attribute = ValidType(caption="valid")
         _invalid_attribute = ValidType(caption="invalid")
@@ -26,7 +26,7 @@ def user_data_container(user_data_model):
         pass
 
     @container_model(
-        MODEL=user_data_model, ICON="container.png", CAPTION='PLUGIN DEVELOPER CONTAINER')
+        model=user_data_model, icon="container.png", caption='PLUGIN DEVELOPER CONTAINER')
     class Container:
         container_valid_attribute = ValidType(caption="valid")
         _container_invalid_attribute = ValidType(caption="invalid")
@@ -38,10 +38,10 @@ def test_data_model(user_data_model):
     import attr
 
     # "data_model" should not have references to others model
-    assert attr.fields(user_data_model).MODEL.default is None
+    assert attr.fields(user_data_model).model.default is None
 
-    assert attr.fields(user_data_model).CAPTION.default == 'PLUGIN DEVELOPER MODEL'
-    assert attr.fields(user_data_model).ICON.default == 'model.png'
+    assert attr.fields(user_data_model).caption.default == 'PLUGIN DEVELOPER MODEL'
+    assert attr.fields(user_data_model).icon.default == 'model.png'
 
     assert attr.fields(user_data_model).valid_attribute is not None
 
@@ -52,11 +52,11 @@ def test_data_model(user_data_model):
 def test_data_container(user_data_container):
     import attr
 
-    assert attr.fields(user_data_container).MODEL.default is not None
-    assert 'Model' in str(attr.fields(user_data_container).MODEL.default)
+    assert attr.fields(user_data_container).model.default is not None
+    assert 'Model' in str(attr.fields(user_data_container).model.default)
 
-    assert attr.fields(user_data_container).CAPTION.default == 'PLUGIN DEVELOPER CONTAINER'
-    assert attr.fields(user_data_container).ICON.default == 'container.png'
+    assert attr.fields(user_data_container).caption.default == 'PLUGIN DEVELOPER CONTAINER'
+    assert attr.fields(user_data_container).icon.default == 'container.png'
 
     assert attr.fields(user_data_container).container_valid_attribute is not None
     assert not hasattr(attr.fields(user_data_container), '_container_invalid_attribute')
