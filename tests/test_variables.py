8eb0e894359203f84f94c9a293e84ffa9f2f5419
William Jamir Silva <williamjamir@gmail.com>
2019-01-31 11:27:20 -0200

    Add parametrize on test_variable

--- tests/test_variables.py
+++ tests/test_variables.py
@@ -1,28 +1,19 @@
 import pytest
 
 
-def test_additional_variable():
-    from alfasim_sdk.variables import Variable, Visibility, Location, Scope
+@pytest.mark.parametrize('attr, expected_type_error, expected_message', [
+    (dict(name=''), ValueError, 'The field "name" cannot be empty'),
+    (dict(caption=''), ValueError, 'The field "caption" cannot be empty'),
+    (dict(unit='foo'), ValueError, 'foo is not a valid unit'),
+    (dict(visibility=''), TypeError, "'visibility' must be <enum 'Visibility'>"),
+    (dict(location=''), TypeError, "'location' must be <enum 'Location'>"),
+    (dict(multifield_scope=''), TypeError, "'multifield_scope' must be <enum 'Scope'>"),
+    (dict(default_value='1'), TypeError, "'default_value' must be <class 'numbers.Real'>"),
 
-    with pytest.raises(ValueError, match='The field "name" cannot be empty'):
-        Variable(name='', caption='caption', unit='m')
-
-    with pytest.raises(ValueError, match='The field "caption" cannot be empty'):
-        Variable(name='name', caption='  ', unit='m')
-
-    with pytest.raises(TypeError, match="'visibility' must be <enum 'Visibility'>"):
-        Variable(name='name', caption='caption', unit='m', visibility='')
-
-    with pytest.raises(TypeError, match="'location' must be <enum 'Location'>"):
-        Variable(name='name', caption='caption', unit='m', location='')
-
-    with pytest.raises(TypeError, match="'multifield_scope' must be <enum 'Scope'>"):
-        Variable(name='name', caption='caption', unit='m', multifield_scope='')
-
-    with pytest.raises(ValueError, match="foo is not a valid unit"):
-        Variable(name='name', caption='caption', unit='foo')
-
-    variable = Variable(
+])
+def test_validation(attr, expected_type_error, expected_message):
+    from alfasim_sdk.variables import Visibility, Location, Scope, SecondaryVariable
+    attrs = dict(
         name='var_name',
         caption='Var Name',
         unit='m',
@@ -31,12 +22,6 @@ def test_additional_variable():
         multifield_scope=Scope.Global,
         checked_on_gui_default=False,
     )
-
-    assert variable.category == 'length'
-    assert variable.default_value == None
-
-    with pytest.raises(TypeError, match="'default_value' must be <class 'numbers.Real'>"):
-        Variable(name='name', caption='caption', unit='m', default_value='1')
-
-    var2 = Variable(name='name', caption='caption', unit='m', default_value=1)
-    assert var2.default_value == 1
+    attrs.update(attr)
+    with pytest.raises(expected_type_error, match=expected_message):
+        SecondaryVariable(**attrs)
