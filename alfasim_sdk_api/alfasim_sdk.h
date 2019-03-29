d581b43b4e501065d670c00db57200ac92f33292
Edson Tadeu M. Manoel <tadeu@esss.co>
2019-03-29 10:00:19 -0300

    Improve support for Linux compilers
    Clang defines `unix` but GCC does not
    
    EDEN-1807

--- alfasim_sdk_api/alfasim_sdk.h
+++ alfasim_sdk_api/alfasim_sdk.h
@@ -2,7 +2,7 @@
 #include <alfasim_sdk_api/detail/api_pointers.h>
 #if defined(_WIN32)
 #include <alfasim_sdk_api/detail/bootstrap_win.h>
-#elif defined(unix)
+#elif defined(unix) || defined(__unix__) || defined(__unix)
 #include <alfasim_sdk_api/detail/bootstrap_linux.h>
 #else
 #error "Unknown host (Alfasim SDK will only work on Linux and Windows)"
