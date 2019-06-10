09d33533f29e5a59cf1a5e9a17a25521b9272696
William Jamir Silva <williamjamir@gmail.com>
2019-06-10 18:02:02 -0300

    Add alfasim_get_status

--- alfasim_sdk/hook_specs_gui.py
+++ alfasim_sdk/hook_specs_gui.py
@@ -17,3 +17,12 @@ def alfasim_get_additional_variables():
     This variable can be used to store internal data from the plugin,
     or it can be used to expose data to the user in the plot window.
     """
+
+
+@hookspec
+def alfasim_get_status(ctx):
+    """
+    Allows plugins to execute custom checks on ALFAsim.
+    This checks can be used to guarantee the consistency of the data,
+    or compatibility with some configuration made on ALFAsim.
+    """
