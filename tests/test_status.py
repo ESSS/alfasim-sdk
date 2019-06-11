450df373cd086c5b12e9e05b2ec85258de76db08
William Jamir Silva <williamjamir@gmail.com>
2019-06-11 11:33:13 -0300

    use status_class instead of status for parameterized option

--- tests/test_status.py
+++ tests/test_status.py
@@ -4,24 +4,24 @@ from alfasim_sdk.status import ErrorMessage
 from alfasim_sdk.status import WarningMessage
 
 
-@pytest.mark.parametrize("status", [WarningMessage, ErrorMessage])
-def test_status(status):
-    out = status(model_name="Test1", message="Message From test")
+@pytest.mark.parametrize("status_class", [WarningMessage, ErrorMessage])
+def test_status(status_class):
+    out = status_class(model_name="Test1", message="Message From test")
     assert out.model_name == "Test1"
     assert out.message == "Message From test"
 
     with pytest.raises(ValueError, match='The field "model_name" cannot be empty'):
-        status(model_name="", message="Message From test")
+        status_class(model_name="", message="Message From test")
 
     with pytest.raises(ValueError, match='The field "message" cannot be empty'):
-        status(model_name="Test", message="")
+        status_class(model_name="Test", message="")
 
     with pytest.raises(
         TypeError, match="'model_name' must be 'str' \(got 42 that is a 'int'\)"
     ):
-        status(model_name=42, message="Foo")
+        status_class(model_name=42, message="Foo")
 
     with pytest.raises(
         TypeError, match="'message' must be 'str' \(got 42 that is a 'int'\)"
     ):
-        status(model_name="Foo", message=42)
+        status_class(model_name="Foo", message=42)
