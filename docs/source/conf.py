1cd967750ccf66f87458d47176b95cca6f1ae380
William Jamir Silva <williamjamir@gmail.com>
2019-10-07 15:20:10 -0300

    add debug for rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,8 @@ from pathlib import Path
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 if on_rtd:
     subprocess.call(
-        "cd ..; doxygen _build/breathe/doxygen/alfasim_sdk_api.cfg", shell=True
+        "cd ..; pwd; ls -lh; doxygen _build/breathe/doxygen/alfasim_sdk_api.cfg",
+        shell=True,
     )
 
 breathe_default_project = "alfasim_sdk_api"
