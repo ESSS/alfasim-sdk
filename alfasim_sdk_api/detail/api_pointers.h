4ac174f45cd5cd566edc8145a341b71767a66e65
Gustavo G. Ribeiro <gribeiro@esss.co>
2020-03-25 10:18:21 -0300

    Add new UCM api functions pointer to the plugins API

--- alfasim_sdk_api/detail/api_pointers.h
+++ alfasim_sdk_api/detail/api_pointers.h
@@ -82,6 +82,8 @@ typedef int (*get_tracer_partition_coefficient_func)(void* ctx, double* out, voi
 typedef int (*get_wall_layer_id_func)(void* ctx, int control_volume, const char* material_name, int* out);
 typedef int (*set_wall_layer_property_func)(void* ctx, int control_volume, int wall_layer_id, enum WallLayerProperty property_id, double new_value);
 typedef int (*get_plugin_input_data_multiplereference_selected_size_func)(void* ctx, int* indexes_size, const char* plugin_id, const char* var_name);
+typedef int (*get_ucm_friction_factor_input_variable_func)(void* ctx, double* out, const char* var_name, int phase_id);
+typedef int (*get_ucm_fluid_geometrical_properties_func)(void* ctx, double** S_w, double* S_i, double** H, double alpha_G, double D, int phase_id);
 
 struct ALFAsimSDK_API {
 #if defined(_WIN32)
@@ -136,6 +138,9 @@ struct ALFAsimSDK_API {
     get_wall_interfaces_temperature_func get_wall_interfaces_temperature;
     get_wall_layer_id_func get_wall_layer_id;
     set_wall_layer_property_func set_wall_layer_property;
+
+    get_ucm_friction_factor_input_variable_func get_ucm_friction_factor_input_variable;
+    get_ucm_fluid_geometrical_properties_func get_ucm_fluid_geometrical_properties;
 
 };
 
