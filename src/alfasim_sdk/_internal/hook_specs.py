4f52ead9500d78091756226c2a43c24560dd342d
Gustavo G. Ribeiro <gribeiro@esss.co>
2021-05-31 17:38:28 -0300

    Update relative emulsion viscosiry hook
    Since it is suppose to implement a correlation, now
    this hook receive double instead of an array of doubles.

--- src/alfasim_sdk/_internal/hook_specs.py
+++ src/alfasim_sdk/_internal/hook_specs.py
@@ -1767,16 +1767,18 @@ def calculate_liq_liq_shear_force_per_volume(
 def calculate_relative_emulsion_viscosity(
     ctx: "void*",
     mu_r: "double*",
+    mu_disp: "double",
+    mu_cont: "double",
+    alpha_disp_in_layer: "double",
     disp_field_index: "int",
-    layer_index: "int",
-    n_faces: "int",
+    cont_field_index: "int",
 ) -> "int":
     """
-    **c++ signature** : ``HOOK_RELATIVE_EMULSION_VISCOSITY(void* ctx, double* mu_r, int disp_field_index, int layer_index, int n_faces)``
+    **c++ signature** : ``HOOK_RELATIVE_EMULSION_VISCOSITY(void* ctx, double* mu_r, double mu_disp, double mu_cont, double alpha_disp_in_layer)``
 
     Internal `hook` to calculate the relative emulsion viscosity in the Emulsion Model.
     This `hook` will be used in the emulsion model to calculate the apparent viscosity of the emulsion
-    (Continuous field + dispersed field).
+    (Continuous field + dispersed field = layer).
 
     The relative emulsion viscosity is defined by:
 
@@ -1793,18 +1795,10 @@ def calculate_relative_emulsion_viscosity(
     :2: :math:`\\mu_m` is the apparent viscosity
     :3: :math:`\\mu_c` is the viscosity of the continuous field
 
-    The output variable ``mu_r`` is the relative emulsion viscosity with size equal to ``n_faces`` and it
-    is dimensionless.
-    The ``disp_field_index`` is the index of the dipersed field. Since it is an emulsion dispersed field,
-    it can have values for ``oil in water`` and ``water in oil`` fields.
-    The ``layer_index`` is the index of the layer and continuous field in the layer. Since it is the index
-    of the main field of the emulsion, it can have values for ``oil``  or ``water`` layers (and/or continuous
-    fields).
-
-    .. Note::
-        It is important to know that the calculations for viscosity is performed over the faces (see
-        ``n_faces`` param). For that, any variable that should be retrieved using :cpp:func:`get_simulation_array`
-        must be use value ``Face`` in the :cpp:enum:`GridScope` param.
+    The output variable ``mu_r`` is the relative emulsion viscosity, ``mu_disp`` is the dispersed field
+    viscosity, ``mu_cont`` is the continuous field viscosity and ``alpha_disp_in_layer`` is the volume
+    fraction of dispersed field in the layer. Finally ``disp_field_index`` and ``cont_field_index`` are
+    the Dispersed and Continuous Field indexes of the emulsion, respectively.
 
     This `hook` allows the implementation of the relative emulsion viscosity correlation. Once the plugin
     installed it is important to be selected in the emulsion model configuration inside the Physics data
@@ -1812,9 +1806,12 @@ def calculate_relative_emulsion_viscosity(
 
     :param ctx: ALFAsim's plugins context
     :param mu_r: Relative emulsion viscosity
+    :param mu_disp: Dispersed field viscosity
+    :param mu_cont: Continuous field viscosity
+    :param alpha_disp_in_layer: Volume fraction of dispersed field in layer.
     :param disp_field_index: Index of the dispersed field
-    :param layer_index: Index of the Layer or Continuous Field
-    :param n_faces: Number of faces.
+    :param cont_field_index: Index of the continuous field
+
     :returns: Return OK if successful or anything different if failed
 
     Example of usage:
@@ -1823,25 +1820,27 @@ def calculate_relative_emulsion_viscosity(
         :linenos:
         :emphasize-lines: 1
 
-        HOOK_RELATIVE_EMULSION_VISCOSITY(ctx, mu_r, disp_field_index, layer_index, n_faces)
+        HOOK_RELATIVE_EMULSION_VISCOSITY(ctx, mu_r, mu_disp, mu_cont, alpha_disp_in_layer)
         {
-            const char* plugin_id = get_plugin_id()
-            errcode = alfasim_sdk_api.get_thread_id(ctx, &thread_id);
+            int water_in_oil_id = -1;
+            errcode = alfasim_sdk_api.get_field_id(ctx, &water_in_oil_id, "water in oil");
             if (errcode != OK) {
                 return errcode;
             }
-            // MyStruct is a developer defined struct to hold
-            // all important information for plugin hooks.
-            MyStruct* data = nullptr;
-            errcode = alfasim_sdk_api.get_plugin_data(ctx, (void**) &data, plugin_id, thread_id);
 
-            if (disp_field_index == data.oil_in_water_index){
+            int oil_in_water_id = -1;
+            errcode = alfasim_sdk_api.get_field_id(ctx, &oil_in_water_id, "oil in water");
+            if (errcode != OK) {
+                return errcode;
+            }
+
+            if (disp_field_index == oil_in_water_id){
                 // Calculate the relative emulsion viscosity
                 // for water dominated scenario.
                 // ComputeForWaterDominated is a function implemented
                 // by plugin developer
-                ComputeForWaterDominated(mu_r, n_faces);
-            } else if (disp_field_index == data.water_in_oil_index){
+                ComputeForWaterDominated(mu_r, mu_disp, mu_cont);
+            } else if (disp_field_index == water_in_oil_id){
                 // Calculate the relative emulsion viscosity
                 // for oil dominated scenario
                 // ComputeForOilDominated is a function implemented
