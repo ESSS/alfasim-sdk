5306f24643263db0b6545670e7dac26804861f3e
Arthur Soprana <arthur@esss.co>
2022-04-29 15:41:05 -0300

    Improved tests

--- tests/test_results_api.py
+++ tests/test_results_api.py
@@ -1,10 +1,15 @@
 from alfasim_sdk import get_profile_from_profile_id
 from alfasim_sdk import get_time_set_and_trend_from_trend_id
 from alfasim_sdk import read_metadata
+from alfasim_sdk import load_result_files
 from alfasim_sdk import RESULTS_FOLDER_NAME
+from alfasim_sdk import META_GROUP_NAME
+from alfasim_sdk import read_profiles_domain_data
+from barril.units import Array
 
+def test_read_results(datadir, num_regression):
+    """"TODO: arthur: Improve/evolve this test"""
 
-def test_read_results(datadir):
     curve_name = "pressure"
 
     results_metadata = read_metadata(datadir / RESULTS_FOLDER_NAME)
@@ -17,18 +22,40 @@ def test_read_results(datadir):
 
     time_set, trend = get_time_set_and_trend_from_trend_id(trend_id, results_metadata)
 
-    time_set_info = results_metadata.time_set_info["profiles"]
-    time_set_key = results_metadata.profiles[profile_id]["time_set_key"]
-    total_time_steps = sum(
-        (
-            info.size
-            for base_ts, info in time_set_info.items()
-            if base_ts in time_set_key
-        ),
-        0,
+    obtained_trends = {
+        "domain [h]" : time_set.GetValues("h"),
+        "image [bar]" : trend.GetValues("bar"),
+    }
+    num_regression.check(
+        obtained_trends,
+        basename="trends"
     )
-    last_ts_index = total_time_steps - 1
-
-    profile = get_profile_from_profile_id(
-        profile_id, results_metadata, timestep_id=last_ts_index
+    
+    def get_last_time_set_index():
+        time_set_info = results_metadata.time_set_info["profiles"]
+        time_set_key = results_metadata.profiles[profile_id]["time_set_key"]
+        total_time_steps = sum(
+            (
+                info.size
+                for base_ts, info in time_set_info.items()
+                if base_ts in time_set_key
+            ),
+            0,
+        )
+        last_ts_index = total_time_steps - 1
+        
+        return last_ts_index
+    
+    profile_domain, profile_image = get_profile_from_profile_id(
+        profile_id, results_metadata, timestep_id=get_last_time_set_index()
+    )
+    
+    
+    obtained_profiles = {
+        "domain [m]" : profile_domain.GetValues("m"),
+        "image [bar]" : profile_image.GetValues("bar"),
+    }
+    num_regression.check(
+        obtained_profiles,
+        basename="profiles"
     )
