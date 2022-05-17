54e0ef548c815c2a98cafc1ef03fcb1cf7573286
Ederson Augusto Grein <ederson@esss.co>
2022-05-17 08:21:42 -0300

    Code review
    	Add assert
    	Add tuning factor curve validation
    
    ASIM-4675

--- src/alfasim_sdk/_internal/alfacase/case_description.py
+++ src/alfasim_sdk/_internal/alfacase/case_description.py
@@ -2648,8 +2648,8 @@ class PhysicsDescription:
 
     emulsion_relative_viscosity_tuning_factor = attrib_curve(
         default=Curve(
-            Array("dimensionless", [1.0], "-"),
-            Array("volume per volume", [0.0], "m3/m3"),
+            image=Array("dimensionless", [1.0], "-"), # tuning factor
+            domain=Array("volume per volume", [0.0], "m3/m3"), # water-cut
         )
     )
 
@@ -2676,6 +2676,17 @@ class PhysicsDescription:
             and value.GetCategory() == "volume per volume"
             and 0.0 <= value.GetValue("m3/m3") <= 1.0
         ), "Invalid inversion point water-cut"
+
+    @emulsion_relative_viscosity_tuning_factor.validator
+    def _validate_emulsion_relative_viscosity_tuning_factor(self, attribute, value):
+        assert isinstance(value, Curve), "Invalid tuning factor curve"
+        domain = value.GetDomain()
+        assert domain.GetCategory() == "volume per volume", "Invalid water-cut category"
+        domain_values = np.asarray(domain.GetValues('m3/m3'))
+        assert np.min(domain_values) >= 0. and np.max(domain_values) <= 1., "Invlid water-cut values"
+        image = value.GetImage()
+        assert image.GetCategory() == 'dimensionless' and np.min(image.GetValues('-')) >= 1.e-5, \
+            "Tuning factor cannot be lower than 1e-5"
 
 
 @attr.s()
