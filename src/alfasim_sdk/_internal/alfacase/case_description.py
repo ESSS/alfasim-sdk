1de8087b69e648d3a8946a83cb877397d760f396
Gustavo Alves <gustavoac@esss.co>
2022-08-17 11:50:53 -0300

    Returning table_type to be a string type

--- src/alfasim_sdk/_internal/alfacase/case_description.py
+++ src/alfasim_sdk/_internal/alfacase/case_description.py
@@ -2243,7 +2243,7 @@ class PvtModelTableParametersDescription:
     number_of_phases: int = attr.ib(default=2, validator=instance_of(int))
     warn_when_outside: bool = attr.ib(default=True, validator=instance_of(bool))
 
-    table_type = attrib_enum(default=constants.PVTTableType.PT)
+    table_type: Optional[str] = attr.ib(default=None, validator=optional(instance_of(str)))
 
     def __attrs_post_init__(self):
         """
