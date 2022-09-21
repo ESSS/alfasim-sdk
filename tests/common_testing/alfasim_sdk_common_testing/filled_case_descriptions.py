f6939d8924aaf24cc28a14860c751a9c30c892c9
Gustavo Alves <gustavoac@esss.co>
2022-09-21 18:01:21 -0300

    build_constant_pvt_table returning a PvtModel

--- tests/common_testing/alfasim_sdk_common_testing/filled_case_descriptions.py
+++ tests/common_testing/alfasim_sdk_common_testing/filled_case_descriptions.py
@@ -79,8 +79,11 @@ PVT_MODEL_COMBINED_DEFINITION = case_description.PvtModelCombinedDescription(
     reference_pvt_model="acme",
     fluids={"combined_fluid_1": COMBINED_FLUID_DESCRIPTION},
 )
-PVT_MODEL_TABLE_PARAMETERS = (
+PVT_MODEL_PT_TABLE_PARAMETERS = (
     case_description.PvtModelTableParametersDescription.create_constant(has_water=True)
+)
+PVT_MODEL_PH_TABLE_PARAMETERS = (
+    case_description.PhPvtModelTableParametersDescription.create_constant(has_water=True)
 )
 PVT_MODELS_DEFINITION = case_description.PvtModelsDescription(
     default_model="acme",
